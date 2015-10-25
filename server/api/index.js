61966ec79ba68a0215f62075d13e8695b8ff64d6
Clement Hoang <clement.hoang24@gmail.com>
2015-10-24 17:24:02 -0700

    Scaffolds some endpoints

--- server/api/index.js
+++ server/api/index.js
@@ -14,5 +14,26 @@ export default function() {
 		});
 	});
 
+	api.post('/login', (req, res) => {
+		// TODO: validate against database
+		var userType = 'student';  // || 'prof';
+
+		res.json({
+			user: 'req.username',
+			role: userType,
+			authenticated: true
+		});
+	});
+
+	api.post('/users', (req, res) => {
+		// make a user based on username, pw, role type
+
+	});
+
+	api.post('/users/:username/classrooms', (req, res) => {
+		// make a classroom, living under prof object, takes in 'access_code', 'classroom_name'
+
+	});
+
 	return api;
 }
